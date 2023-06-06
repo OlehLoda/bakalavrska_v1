08c41b602e01bae00e2b219caa17cbbe8f094e33
OlehLoda <oleh.loda@surelockkey.com>
2023-06-06 03:02:24 +0300

    NEXTAUTH_URL

--- app/NextAuthWrapper.tsx
+++ app/NextAuthWrapper.tsx
@@ -6,7 +6,8 @@ export default function NextAuthProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const nextAuthUrl = process.env.NEXTAUTH_URL;
+  // const nextAuthUrl = process.env.NEXTAUTH_URL;
+  const nextAuthUrl = process.env.NEXTAUTH_URL_VERCEL;
 
   return <SessionProvider baseUrl={nextAuthUrl}>{children}</SessionProvider>;
 }
